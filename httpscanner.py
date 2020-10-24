1219608382b8fcf7bd4dca54d82789a8e8d74563
FlingJLJ <60308125+FlingJLJ@users.noreply.github.com>
2020-10-23 20:33:57 -0500

    Update httpscanner.py

--- httpscanner.py
+++ httpscanner.py
@@ -1,10 +1,5 @@
-import pdb
 import os
 import requests as req
-import tkMessageBox
-
-wlist = ["js", "admin", "users", "src"]
-url = input("Enter URL to scan: ")
 
 def clear():
 	if os.name == "nt":
@@ -12,9 +7,10 @@ def clear():
 	else:
 		os.system("clear")
 
-def fatalErr(message):
-	tkMessageBox.showinfo(f"Error in {__file__.name}", f"A fatal error occured and the program must be stopped.\nError message:\n{message}")
+wlist = ["js", "admin", "users", "src"]
+url = input("Enter URL to scan: ")
 
+clear()
 print(f"""-- HTTPScanner --
 ----------------------------------
 URL: {url}
@@ -24,7 +20,9 @@ for word in wlist:
 	try:
 		r = req.get(f"https://{url}/{word}")
 	except:
-		fatalErr("Invalid URL requested.")
+		print("Invalid url, press enter to exit.")
+		input()
+		exit()
 	if r.status_code != 404 | r.status_code != 403:
 		print(f"Found: /{word} | Status: {r.status_code}")
 	if r.status_code == 403:
@@ -34,4 +32,3 @@ for word in wlist:
 print("Scanning done.")
 
 input()
-breakpoint()
