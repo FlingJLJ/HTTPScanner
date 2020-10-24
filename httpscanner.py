ade94ada9a618497ef015f20190644deb1310169
FlingJLJ <60308125+FlingJLJ@users.noreply.github.com>
2020-10-23 20:11:37 -0500

    everything

--- httpscanner.py
+++ httpscanner.py
@@ -1,33 +1,37 @@
+import pdb
+import os
 import requests as req
-import threading
+import tkMessageBox
 
 wlist = ["js", "admin", "users", "src"]
 url = input("Enter URL to scan: ")
-global found
-found = 0
 
-def scan(word, url):
-	r = req.get(f"https://{url}/{word}")
-	if len(r.history) > 1:
-		return
+def clear():
+	if os.name == "nt":
+		os.system("cls")
+	else:
+		os.system("clear")
+
+def fatalErr(message):
+	tkMessageBox.showinfo(f"Error in {__file__.name}", f"A fatal error occured and the program must be stopped.\nError message:\n{message}")
+
+print(f"""-- HTTPScanner --
+----------------------------------
+URL: {url}
+----------------------------------""")
+
+for word in wlist:
+	try:
+		r = req.get(f"https://{url}/{word}")
+	except:
+		fatalErr("Invalid URL requested.")
 	if r.status_code != 404 | r.status_code != 403:
-		print(f"Page found: /{word} Status: {r.status_code}")
-		found += 1
+		print(f"Found: /{word} | Status: {r.status_code}")
 	if r.status_code == 403:
 		print("Error: You have been denied access to this web server.")
 		input("Press the ENTER key to attempt to continue.")
 
-print(f"""
--- HTTPScanner --
-----------------------------------
-URL: {url}
-----------------------------------
-""")
-
-for word in wlist:
-	threading.Thread(target=scan(word, url))
-
-if found == 0:
-	print("Nothing found. :'(")
+print("Scanning done.")
 
 input()
+breakpoint()
