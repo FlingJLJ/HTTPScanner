f32f1feb9b34db30f13040b848eefff7804f9e28
FlingJLJ <60308125+FlingJLJ@users.noreply.github.com>
2020-10-23 22:13:12 -0500

    Update httpscanner.py

--- httpscanner.py
+++ httpscanner.py
@@ -1,18 +1,10 @@
 import os
 import requests as req
 
-def clear():
-	if os.name == "nt":
-		os.system("cls")
-	else:
-		os.system("clear")
-
 wlist = ["js", "admin", "users", "src"]
 
-clear()
 url = input("Enter URL to scan: ")
 
-clear()
 print(f"""-- HTTPScanner --
 ----------------------------------
 URL: {url}
@@ -20,17 +12,16 @@ URL: {url}
 
 for word in wlist:
 	try:
-		r = req.get(f"https://{url}/{word}")
+		r = req.get(f"https://{url}/{word}") # attempt to get
 	except:
 		print("Invalid url, press enter to exit.")
 		input()
 		exit()
-	if r.status_code != 404 | r.status_code != 403:
+	if r.status_code != 404 | r.status_code != 403: # remove irrelevant responses
 		print(f"Found: /{word} | Status: {r.status_code}")
-	if r.status_code == 403:
+	if r.status_code == 403: # alert user on potential IP ban
 		print("Error: You have been denied access to this web server.")
 		input("Press the ENTER key to attempt to continue.")
 
 print("Scanning done.")
-
-input()
+input("Press any key to exit...")
